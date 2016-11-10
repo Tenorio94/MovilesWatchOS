b6761f11e8e52de3e45cc5c5ba60f8c78d26987f
Tenorio94 <geteca94@gmail.com>
2016-11-10 11:52:05 -0600

    Prepares last interface
    - Confirm Interface prepared
    - Changes structure to labels

--- watchOSInvestigation/watchOSInvestigation WatchKit Extension/ConfirmController.swift
+++ watchOSInvestigation/watchOSInvestigation WatchKit Extension/ConfirmController.swift
@@ -12,15 +12,30 @@ import WatchKit
 class ConfirmController: WKInterfaceController {
 
     @IBOutlet var labelResumen: WKInterfaceLabel!
+    @IBOutlet var labelPollo: WKInterfaceLabel!
+    @IBOutlet var labelJamon: WKInterfaceLabel!
+    @IBOutlet var labelRes: WKInterfaceLabel!
+    @IBOutlet var labelSalchicha: WKInterfaceLabel!
+    @IBOutlet var labelTocino: WKInterfaceLabel!
+    
     
     var sSummary: String = ""
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
+        
         if let dataReceived = context as? Data{
             sSummary = dataReceived.sBreadType
             labelResumen.setText(sSummary)
+            
+            dataReceived.bPollo ? labelPollo.setText("Pollo") : labelPollo.setHidden(true)
+            dataReceived.bJamon ? labelJamon.setText("Jamon") : labelJamon.setHidden(true)
+            dataReceived.bRes ? labelRes.setText("Res") : labelRes.setHidden(true)
+            dataReceived.bSalchicha ? labelSalchicha.setText("Salchicha") : labelSalchicha.setHidden(true)
+            dataReceived.bTocino ? labelTocino.setText("Tocino") : labelTocino.setHidden(true)
+            
+            
         }
         // Configure interface objects here.
     }
